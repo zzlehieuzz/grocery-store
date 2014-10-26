14fb6af6ca3952376d8191109c6797296a748835
zzlehieuzz <zzlehieuzz@gmail.com>
2014-10-27 00:47:33 +0700

    remove

--- src/Sof/ApiBundle/Entity/ValueConst/PatapataChanceMasterConst.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Sof\ApiBundle\Entity\ValueConst;
-use Sof\ApiBundle\Entity\BaseEntity;
-
-class PatapataChanceMasterConst extends BaseEntity
-{
-    const PROCESSING_PATAPATA  = 1; //1: ぱたぱた
-    const PROCESSING_CUBE      = 3; //2:Cube
-}
--- src/Sof/ApiBundle/Entity/ValueConst/SystemOperationMasterConst.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Sof\ApiBundle\Entity\ValueConst;
-use Sof\ApiBundle\Entity\BaseEntity;
-
-class SystemOperationMasterConst extends BaseEntity
-{
-
-}
