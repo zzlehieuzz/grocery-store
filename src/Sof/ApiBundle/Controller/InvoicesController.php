3b1401acd82c65fa4bf6f570173167138b517b29
TRAN QUANG THONG <quangthongtran@gmail.com>
2014-11-08 17:43:34 +0700

    Adding Invoice module

--- src/Sof/ApiBundle/Controller/InvoicesController.php
+++ src/Sof/ApiBundle/Controller/InvoicesController.php
@@ -10,6 +10,7 @@ use Sof\ApiBundle\Lib\DateUtil;
 
 class InvoicesController extends BaseController
 {
+    //LIST INVOICE
 
     /**
      * @Route("/Invoice_Load", name="Invoice_Load")
@@ -25,8 +26,6 @@ class InvoicesController extends BaseController
             unset($entity['createInvoiceDate']);
             $arrData[] = $entity;
         }
-
-//        var_dump($arrData); die;
 
         return $this->jsonResponse(array('data' => $arrData));
     }
@@ -50,5 +49,134 @@ class InvoicesController extends BaseController
       $entityService->completeTransaction();
 
       return $this->jsonResponse(array('data' => $params));
+    }
+
+    //PHIEU NHAP
+    /**
+     * @Route("/Input_Load", name="Input_Load")
+     */
+    public function Input_LoadAction()
+    {
+        //Phiếu Nhập: type= 1
+
+        $arrData = array();
+
+        if ($id = 1) {
+            $arrEntity = $this->getEntityService()->getAllData(
+                'Invoice',
+                array('orderBy' => array('id' => 'DESC')));
+
+            foreach($arrEntity as $entity){
+                unset($entity['createInvoiceDate']);
+                $arrData[] = $entity;
+            }
+        }
+
+        return $this->jsonResponse(array('data' => $arrData));
+    }
+
+    /**
+     * @Route("/Input_Update", name="Input_Update")
+     */
+    public function Input_UpdateAction()
+    {
+//        $params        = $this->getJsonParams();
+        $params        =  $this->get('request')->getContent();
+        $params = json_decode($params);
+        $form_fields_value = $params->form_fields_value;
+        $grid_value = $params->grid_value;
+
+//        echo "<pre>";
+//        print_r($form_fields_value[0]);
+//        print_r($grid_value); die;
+
+        $arrEntity = $this->getEntityService()->getAllData(
+            'Invoice',
+            array('orderBy' => array('id' => 'DESC')));
+
+        $arrData = array();
+        foreach($arrEntity as $entity){
+            unset($entity['createInvoiceDate']);
+            $arrData[] = $entity;
+        }
+
+        return $this->jsonResponse(array('data' => $arrData));
+    }
+
+    /**
+     * @Route("/Input_Delete", name="Input_Delete")
+     */
+    public function Input_DeleteAction()
+    {
+        $arrEntity = $this->getEntityService()->getAllData(
+            'Invoice',
+            array('orderBy' => array('id' => 'DESC')));
+
+        $arrData = array();
+        foreach($arrEntity as $entity){
+            unset($entity['createInvoiceDate']);
+            $arrData[] = $entity;
+        }
+
+        return $this->jsonResponse(array('data' => $arrData));
+    }
+
+    //PHIEU XUAT
+    /**
+     * @Route("/Output_Load", name="Output_Load")
+     */
+    public function Output_LoadAction()
+    {
+        //Phiếu Xuất: type= 2
+        $arrData = array();
+
+        if ($id = 1) {
+            $arrEntity = $this->getEntityService()->getAllData(
+                'Invoice',
+                array('orderBy' => array('id' => 'DESC')));
+
+            foreach($arrEntity as $entity){
+                unset($entity['createInvoiceDate']);
+                $arrData[] = $entity;
+            }
+        }
+
+        return $this->jsonResponse(array('data' => $arrData));
+    }
+
+    /**
+     * @Route("/Output_Update", name="Output_Update")
+     */
+    public function Output_UpdateAction()
+    {
+        $arrEntity = $this->getEntityService()->getAllData(
+            'Invoice',
+            array('orderBy' => array('id' => 'DESC')));
+
+        $arrData = array();
+        foreach($arrEntity as $entity){
+            unset($entity['createInvoiceDate']);
+            $arrData[] = $entity;
+        }
+
+        return $this->jsonResponse(array('data' => $arrData));
+    }
+
+    /**
+     * @Route("/Output_Delete", name="Output_Delete")
+     */
+    public function Output_DeleteAction()
+    {
+        $arrEntity = $this->getEntityService()->getAllData(
+            'Invoice',
+            array('orderBy' => array('id' => 'DESC')));
+
+        $arrData = array();
+        foreach($arrEntity as $entity){
+            unset($entity['createInvoiceDate']);
+            $arrData[] = $entity;
+        }
+
+        return $this->jsonResponse(array('data' => $arrData));
     }
 }
