76ff09d146a04e7947fdf6f53c758c2bf3f89db7
TRAN QUANG THONG <quangthongtran@gmail.com>
2014-11-14 14:53:08 +0700

    Adding Invoice module

--- web/js/desktop/pages/Invoices/List.js
+++ web/js/desktop/pages/Invoices/List.js
@@ -9,10 +9,13 @@ var readerJson = {
 };
 
 var objectField = [{name: 'id',   type: 'int'},
-                   {name: 'subject', type: 'int'},
+                   {name: 'subjectName', type: 'string'},
                    {name: 'invoiceType', type: 'int'},
-                   {name: 'invoiceNumber', type: 'string'}/*,
-                   {name: 'createInvoiceDate', type: 'string'}*/];
+                   {name: 'invoiceTypeText', type: 'string'},
+                   {name: 'invoiceNumber', type: 'string'},
+                   {name: 'paymentStatus', type: 'string'},
+                   {name: 'amount', type: 'INT'},
+                   {name: 'createInvoiceDate', type: 'string'}];
 
 MyUtil.Object.defineModel('Invoice', objectField);
 
@@ -54,6 +57,7 @@ Ext.define('SrcPageUrl.Invoices.List', {
     init : function(){
         this.launcher = {
             text: 'invoices management'.Translator('Module'),
+//            text: 'Quản lý nhập xuất',
             iconCls:'icon-grid'
         };
     },
@@ -62,17 +66,67 @@ Ext.define('SrcPageUrl.Invoices.List', {
         var desktop = this.app.getDesktop();
         var win = desktop.getWindow('grid-win');
 
+        var formFieldsList = {
+            xtype: 'fieldset',
+            columnWidth: 0.4,
+            labelWidth: 200,
+            defaultType: 'textfield',
+            defaults: {
+                anchor: '100%'
+            },
+            layout: 'anchor',
+            items: [{
+                xtype: 'radiogroup',
+                anchor: '80%',
+                fieldLabel: 'Loại Phiếu',
+                columns: 3,
+                name: 'invoiceTypeRadio',
+                id: 'invoiceTypeRadio',
+                vertical: true,
+                items: [
+                    {boxLabel: 'All', name: 'rb', inputValue: '0', checked: true},
+                    {boxLabel: 'Phiếu Nhập', name: 'rb', inputValue: '1'},
+                    {boxLabel: 'Phiếu Xuất', name: 'rb', inputValue: '2'}
+                ]
+            }, {
+                fieldLabel: 'Từ Ngày',
+                xtype: 'datefield',
+                name: 'fromDate',
+                id: 'fromDate',
+                anchor: '40%'
+            }, {
+                fieldLabel: 'Đến Ngày',
+                name: 'toDate',
+                id: 'toDate',
+                xtype: 'datefield',
+                anchor: '40%'
+            },{
+                xtype: 'button',
+                text: 'Tìm',
+                anchor: '10%',
+                handler : function() {
+
+                    var invoiceType = Ext.getCmp('invoiceTypeRadio').getValue().rb;
+                    var fromDate = Ext.util.Format.date(Ext.getCmp('fromDate').getValue(), 'Y-m-d');
+                    var toDate = Ext.util.Format.date(Ext.getCmp('toDate').getValue(), 'Y-m-d');
+
+                    storeLoadInvoice.reload({params:{limit: 5, page: 1, start: 1, invoiceType: invoiceType, fromDate: fromDate, toDate: toDate}});
+                }
+            }]
+
+        };
+
         var columnsInvoice = [
             new Ext.grid.RowNumberer(),
             {
 //                text: "subject".Translator('Common'),
                 text: "Tên Khách Hàng",
                 width: 100,
-                dataIndex: 'subject',
+                dataIndex: 'subjectName',
                 editor: {
                     allowBlank: true
                 }
-            }, /*{
+            }, {
 //                text: "create_invoice_date".Translator('Invoice'),
                 text: "Ngày",
                 flex: 1,
@@ -80,11 +134,19 @@ Ext.define('SrcPageUrl.Invoices.List', {
                 editor: {
                     allowBlank: true
                 }
-            },*/ {
+            }, {
+//                text: "create_invoice_date".Translator('Invoice'),
+                text: "Thành Tiền",
+                flex: 1,
+                dataIndex: 'amount',
+                editor: {
+                    allowBlank: true
+                }
+            }, {
 //                text: "create_invoice_date".Translator('Invoice'),
                 text: "Loại Phiếu",
                 flex: 1,
-                dataIndex: 'invoiceType',
+                dataIndex: 'invoiceTypeText',
                 editor: {
                     allowBlank: true
                 }
@@ -96,44 +158,37 @@ Ext.define('SrcPageUrl.Invoices.List', {
                 editor: {
                     allowBlank: true
                 }
-            },/*{
-                xtype:'actioncolumn',
-                width:100,
-                items: [{
-//                    icon: 'images/edit.png',  // Use a URL in the icon config
-//                    tooltip: 'Edit',
-                    launch: function(grid, rowIndex, colIndex) {
-//                        var rec = grid.getStore().getAt(rowIndex);
-//                        alert("Edit " + rec.get('firstname'));
+            },
+            {
+                text: '',
+                renderer: function(val,meta,rec) {
+                    var id = Ext.id();
+                    Ext.defer(function() {
+                        Ext.widget('button', {
+                            renderTo: id,
+                            text: 'Xem Chi Tiết',
+                            scale: 'small',
+                            handler: function() {
+                                var invoiceId = rec.data.id;
+                                var invoiceType2 = rec.data.invoiceType;
 
-                        var a = desktop.getWindow(new SrcPageUrl.Invoices.Input());
-                        a.getActiveWindow();
-//                        a.show();
-//                        return a;
-//                        return new SrcPageUrl.Invoices.Input().createWindow();
-                    }
-                }*//*, {
-                    xtype: 'button',
-                    text: 'Xem',
-                    width: 50,
-                    scale: 'small',
-                    handler: function() {
-                        alert("Hello World!");
-                    }
-                }*//*]
-            },*/ {
-//                text: "create_invoice_date".Translator('Invoice'),
-                text: "Xem Chi Tiết",
-                flex: 2,
-//                dataIndex: 'invoiceNumber',
-                editor: {
-                    allowBlank: true
+                                if (invoiceType2 == 1) {
+                                    Ext.Msg.alert("Xem chi tiết Phiếu Nhập ID: " + invoiceId)
+                                } else {
+                                    Ext.Msg.alert("Xem chi tiết Phiếu Xuất ID: " + invoiceId)
+                                }
+
+                            }
+                        });
+                    }, 50);
+
+                    return Ext.String.format('<div id="{0}"></div>', id);
                 }
             }, {
 //                text: "create_invoice_date".Translator('Invoice'),
                 text: "Trạng Thái",
                 flex: 2,
-//                dataIndex: 'invoiceNumber',
+                dataIndex: 'paymentStatus',
                 editor: {
                     allowBlank: true
                 }
@@ -150,18 +205,21 @@ Ext.define('SrcPageUrl.Invoices.List', {
         if(!win){
             win = desktop.createWindow({
                 id: 'invoice-list',
-                title:'invoice management'.Translator('Module'),
+//                title:'invoice management'.Translator('Module'),
+                title:'Quản lý nhập xuất',
                 width:600,
-                height:480,
+                height:500,
                 iconCls: 'icon-grid',
                 animCollapse:false,
                 constrainHeader:true,
-                layout: 'fit',
+//                layout: 'fit',
                 items: [
+                    formFieldsList,
                   {
                     border: false,
                     id: 'grid-invoice-list',
                     xtype: 'grid',
+                    height:300,
                     store: storeLoadInvoice,
                     selModel: rowModel,
                     columns: columnsInvoice,
